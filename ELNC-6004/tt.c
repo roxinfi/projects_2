23437475beb0f3fc4bec20d6dd56d718636e8fb1
roxinfi <v_patel233507@fanshaweonline.ca>
2024-05-17 14:12:35 -0400

    update

---
+++ ELNC-6004/tt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int temprature = 61;
+    char unit = 'C';
+    float result;
+
+    printf("enter the result :");
+    scanf("%f", &result);
+    printf("the result is : %f\n", result);
+    printf("the temprature is : %d\n", temprature);
+    printf("the unit is : %c\n", unit);
+    printf("the temprature is : %x\n", temprature);
+    printf("Hello World\n");
+    return 0;
+}
