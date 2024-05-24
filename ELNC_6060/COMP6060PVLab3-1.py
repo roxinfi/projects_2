d7ce1c89de79d5d54d3761771095771c9d463cbc
roxinfi <v_patel233507@fanshaweonline.ca>
2024-05-23 22:10:24 -0400

    updated 2024-05-23

---
+++ ELNC_6060/COMP6060PVLab3-1.py
@@ -0,0 +1,20 @@
+# Welcome message personalized with your name
+print("Welcome to VRAJ's temperature converter!")
+
+# Prompt the user for a temperature in Celsius
+tempCelsiusStr = input("Please enter a temperature value in Celsius: ")
+
+# Convert the input string to a float
+tempCelsius = float(tempCelsiusStr)
+
+# Convert the Celsius temperature to Fahrenheit using the formula
+tempFahrenheit = (tempCelsius * 9.0/5.0) + 32
+
+# Create a formatted string to display the result
+resultStr = f"{tempCelsius} Celsius = {tempFahrenheit:.2f} Fahrenheit"
+
+# Print the result string
+print(resultStr)
+
+# Thank the user for using the program
+print("Thanks for using the Temperature Converter program!")
---
+++ ELNC_6060/COMP6060PVlab3-2.py
@@ -0,0 +1,26 @@
+# Prompt to welcome the user to the time converter
+print("Welcome to VRAJ's Time Converter!")
+
+# Prompt user for the number of seconds to be converted
+seconds_input = input("Please enter the number of seconds to be converted: ")
+# Casting the input to an integer
+total_seconds = int(seconds_input)
+
+# Calculate total days from the seconds provided
+days = total_seconds // (24 * 60 * 60)
+remainder = total_seconds % (24 * 60 * 60)
+
+# Calculating total hours from the remainder
+hours = remainder // (60 * 60)
+
+# Updating remainder
+remainder %= (60 * 60)
+
+# Calculating total minutes from the updated remainder
+minutes = remainder // 60
+
+# Calculating remaining seconds
+seconds = remainder % 60
+
+# Printing the results in the specified format of Days: Hours: Minutes: Seconds
+print(f"{total_seconds} is equivalent to {days} days {hours:02}:{minutes:02}:{seconds:02}")
