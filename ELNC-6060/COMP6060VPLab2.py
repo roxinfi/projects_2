8f23d962706cd96be294ef32949588a9272ddda3
roxinfi <v_patel233507@fanshaweonline.ca>
2024-05-24 00:36:22 -0400

    updated COMP6060VPLab2.py

--- ELNC-6060/COMP6060VPLab2.py
+++ ELNC-6060/COMP6060VPLab2.py
@@ -2,22 +2,22 @@
 print("welcome to VRAJ Shopping Helpper")
 
 # procedure -3 \\ get the name of the item to be purched
-itemname = input("please enter the quantity of the item to be purched:") # Procedure -3
+itemname = input("please enter the item name to be purched : ") # Procedure -3
 
 # Procedure -4 \\ get the quantity of the item to be purched
-itemQuantityStr = input("please enter the quantity of the item to be purched:") # Procedure -4
+itemQuantityStr = input("please enter the quantity of the item to be purched : ") # Procedure -4
 
 # Procedure -5 \\ get the price of the item to be purched
 itemQuantity = int(itemQuantityStr) # Procedure -5
 
 # Procedure -6 \\ get the price of the item to be purched
-itemPriceStr = input("please enter the price of the item:") # Procedure -6
+itemPriceStr = input("please enter the price of the item : ") # Procedure -6
 
 # Procedure -7 \\ get the price of the item to be purched
 itemPrice = float(itemPriceStr) # Procedure -7
 
 # Procedure -8 \\ get the average weight of the item to be purched
-itemAvgWeightStr = input("please enter the average weight of the item:") # Procedure -8
+itemAvgWeightStr = input("please enter the average weight of the item : ") # Procedure -8
 
 # Procedure -9 \\ get the average weight of the item to be purched
 itemAvgWeight = float(itemAvgWeightStr) # Procedure -9
@@ -29,7 +29,7 @@ totalPrice = itemQuantity * itemPrice # Procedure -10
 totalweighht = itemQuantity * itemAvgWeight # Procedure -11
 
 # Procedure -12 \\ print the total price of the item
-print("Total price of the item is:", totalPrice) # Procedure -12
+print("Your quantity item(s) at $",itemPrice,"each will cost $",totalPrice) # Procedure -12
 
 # Procedure -13 \\ print the total weight of the item
-print("Total weight of the item is:", totalweighht) # Procedure -13
+print("Your quantity item(s) at ",itemAvgWeight,"lbs each will weigh ",totalweighht,"lbs") # Procedure -13
