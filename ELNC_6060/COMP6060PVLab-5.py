d03a59da50321526dc5b2060c89144617dbb108e
roxinfi <v_patel233507@fanshaweonline.ca>
2024-06-06 11:17:38 -0400

    updated file

--- ELNC_6060/COMP6060PVLab-5.py
+++ ELNC_6060/COMP6060PVLab-5.py
@@ -5,19 +5,26 @@ print("Welcome to VRAJ's random number generator")
 # Get the number of values to generate
 numValueStr = input("Enter the number of values to generate between 1 and 50: ")
 numValue = int(numValueStr)
-def getValidUserInput(string, minimumrange, maximumrange): # Function to get valid user input
-        while True: #  Loop until a valid input is entered
+
+# Function to get valid user input
+def getValidUserInput(string, minimumrange, maximumrange): 
+        #  Loop until a valid input is entered
+        while True: 
             string = numValueStr
             if not string.isnumeric() : # Check if the input is a number
                 print("Value entered was not a number. Exiting...")
                 exit()
+                
             num = int(string)
-            if num < minimumrange or num > maximumrange: # Check if the input is within the range
+            # Check if the input is within the range
+            if num < minimumrange or num > maximumrange: 
                 print(f"Please enter a value between {minimumrange} and {maximumrange}")
-            else: # If the input is valid, generate the random numbers
+            # If the input is valid, generate the random numbers, print them and exit the loop
+            else:
                 for num in range(0, numValue):
                     randomVal = int(random() * 100)
                     print(f'{num+1} : {randomVal}')
+                break
 
-
-getValidUserInput(numValueStr, 1, 50) # Call the function to get valid user input
+# Call the function to get valid user input
+getValidUserInput(numValueStr, 1, 50)
