729f1657e2ac026eebbf14bf0453eb77b61653fe
roxinfi <v_patel233507@fanshaweonline.ca>
2024-06-13 11:30:31 -0400

    updated

--- ELNC_6060/COMP6060PVLab-6.py
+++ ELNC_6060/COMP6060PVLab-6.py
@@ -3,11 +3,11 @@ print("Welcome to VRAJ's statistical analysis program")
 # creating a dataset
 dataset = [56,74,-10,58,4,17,26,0,13,37]
 
-# printing the dataset
+# printing the dataset_size
 dataset_size = len(dataset)
 print(f'Dataset size has {dataset_size} elements')
 
-# removing negative values
+# removing negative values from the dataset
 for i in range(dataset_size):
     if dataset[i] < 0:
         dataset[i] = 0
@@ -22,5 +22,15 @@ for val in dataset:
 
 
 # calculating the mean of the dataset
-mean = float(dataset_sum / dataset_size)
+mean = int(dataset_sum / dataset_size)
 print(f'The mean of the dataser = {mean}')
+
+# calculating the median of the dataset
+if dataset_size % 2 == 0:   # if the dataset size is even
+    mid_value_1 = dataset[dataset_size//2]
+    mid_value_2 = dataset[(dataset_size//2) + 1]
+    median = int((mid_value_1 + mid_value_2) / 2)
+    print(f'The median of the dataset = {median}')
+else:                       # if the dataset size is odd
+    median = int(dataset[dataset_size//2])
+    print(f'The median of the dataset = {median}')
