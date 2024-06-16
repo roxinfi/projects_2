da6e78105f202ce2410e09842f5e7fd45b652188
roxinfi <v_patel233507@fanshaweonline.ca>
2024-06-16 15:25:51 -0400

    PIC controller testing

---
+++ junk_test/test_c-2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+
+void delay(int milliseconds) 
+{
+    long pause;
+    clock_t now, then;
+
+    pause = milliseconds * (CLOCKS_PER_SEC / 1000);
+    now = then = clock();
+    while((now - then) < pause)
+        now = clock();
+}
+
+void print_binary(unsigned char num) 
+{
+    for (int i = 7; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    unsigned char counter = 0x09;
+    unsigned char led = 0x00;
+    unsigned char display = 0x00;
+
+    while (1) 
+    {
+        led = 0x00;
+        if (_kbhit()) 
+        {
+            char ch = _getch();
+            if (ch == '1') 
+            {
+                counter--;
+                if (counter == 0x00) 
+                {
+                    counter = 0x09;
+                }
+            } 
+            if (ch == '2') 
+            {
+                if (led == 0x00) 
+                {
+                    led = 0x20;
+                } 
+                else 
+                {
+                    led = 0x00;
+                }
+            }
+        }
+
+        display = (counter) | (led);
+        printf("The counter is: ");
+        print_binary(display);
+        delay(550);
+    }
+
+    return 0;
+}
