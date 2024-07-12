8f9562dbe63b4cb23bbeb194d120066ade314bc2
roxinfi <v_patel233507@fanshaweonline.ca>
2024-07-12 13:25:48 -0400

    update local

--- ELNC_6006_LEARN/pointer_leran.c
+++ ELNC_6006_LEARN/pointer_leran.c
@@ -6,11 +6,12 @@ int main() {
     p = &a;
     
     printf("Value of a: %d\n", a);                  // Outputs: Value of a: 10
-    printf("Address of a: %p\n", (void*)&a);        // Outputs the address of a
-    printf("Value of p: %p\n", (void*)p);           // Outputs the address stored in p (which is the address of a)
+    printf("Address of a: %p\n", &a);        // Outputs the address of a
+    printf("Value of p: %p\n", p);           // Outputs the address stored in p (which is the address of a)
+    printf("Address of p: %p\n", &p);        // Outputs the address of pointer p
     printf("Value of *p: %d\n", *p);                // Outputs: Value pointed to by p: 10
-    printf("Address of p: %p\n", (void*)&p);        // Outputs the address of pointer p
-
+    printf("Address of p: %p\n", &*p);        // Outputs the address of pointer p
+    printf("The value of *p + 10 is %p + %p = %p\n", *p, a , (*p + 10)); // Outputs the value of *p + 10
     // Correct format specifiers for integers
     printf("The sum of a and *p: %p and %p is %p\n", a, (void*)&p, a + *p);
 
